cd018df656f54b8dceb0abcf4293774fcaa34cea
Sadra Sahraei <sadrasahraei04@gmail.com>
2024-05-02 10:08:13 -0700

    first commit

---
+++ app/calculador.py
@@ -0,0 +1,25 @@
+
+class Calculador(object):
+
+    
+    def calculation(self, calc):
+
+        return self.__calculation_validation(calc=calc)
+
+    def __calculation_validation(self, calc):
+
+
+        try:
+            result = eval(calc)
+
+            return self.__format_result(result=result)
+        except (NameError, ZeroDivisionError, SyntaxError, ValueError):
+            return 'Erro' 
+
+    def __format_result(self, result):
+
+        result = str(result)
+        if len(result) > 15:
+            result = '{:5.5E}'.format(float(result))
+            
+        return result
